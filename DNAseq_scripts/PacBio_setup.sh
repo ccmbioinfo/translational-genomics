81e45a9f96f689a918fab7d35f1c09e82878e032
Madeline Couse <mhcouse@gmail.com>
2025-09-02 13:15:00 -0400

    A number of changes: - Make compatible with filenames from updated PacBio pipelne - Check if there are fewer than 3 family members; if so, don't run TRGT de novo - Verify that all family members with sequence data are represented in the pedigree file

--- DNAseq_scripts/PacBio_setup.sh
+++ DNAseq_scripts/PacBio_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#usage: sh PacBio_setup.sh <analyses> <iRods_query_date> <project>
+#usage: sh PacBio_setup.sh <analyses> <project>
 
 set -euo pipefail
 
@@ -27,7 +27,7 @@ fi
 
 CREDS="PT_credentials.csv"
 METADATA_DIR="/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}"
-ANALYSIS_DIR="/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/analyses/${project}"
+ANALYSIS_DIR="/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/analyses/test/${project}"
 CRG2_PACBIO=~/crg2-pacbio
 
 if [ -z $analyses ]; then
@@ -41,6 +41,16 @@ module load bcftools
 
 #python3 get_HPO_pedigree_genome_clinic.py -sample_sheet $analyses -credentials $CREDS
 
+for project_family in `awk '{print $3}' $analyses | cut -d '.' -f1 | tr -d '_' | uniq`
+do
+        echo $project_family
+        if [ -d ${ANALYSIS_DIR}/${project_family}/PacBio ]; then
+                echo "Analysis directory already exists for $project_family. Re-creating samples.tsv and units.tsv files."
+                # now re-initialize sample file
+                echo "sample" > "${ANALYSIS_DIR}/${project_family}/PacBio/samples.tsv"
+        fi
+done
+
 while IFS=$'\t' read -r family sequence_id project_id sample_type status notes uploaded_to_DNAStack lab LIMS
 do
         project_id=`echo $project_id | tr -d '_' | tr '.' '_' | tr -d '\r'` # e.g. convert DSK_018.03 to DSK018_03 so crg2 doesn't mess up wildcards
@@ -51,7 +61,7 @@ do
 
     if [ "$family" != "Family_ID" ]; then
         # create analysis directory for family
-        if [ ! -d ${ANALYSIS_DIR}/${project_family} ]; then
+        if [ ! -d ${ANALYSIS_DIR}/${project_family}/PacBio ]; then
                 echo "Setting up analysis directory for $family"
                 mkdir -p ${ANALYSIS_DIR}/${project_family}/PacBio
                 # copy pipeline files to analysis directory 
@@ -59,24 +69,24 @@ do
                 sed -i "s/NA12878/$project_family/" ${ANALYSIS_DIR}/${project_family}/PacBio/config.yaml
 
                 # add HPO terms to config.yaml
-                echo "Finding HPO terms"
-                HPO=`ls /hpf/largeprojects/tgnode/sandbox/mcouse_analysis/HPO/${project}/${project_family}* | tail -n 1` # last file is the most recent
-                echo $HPO
-                if [ -z $HPO ]; then
-                        echo "No HPO terms found"
-                fi
-                sed -i "s+hpo: \"\"+hpo: \"${HPO}\"+"  ${ANALYSIS_DIR}/${project_family}/PacBio/config.yaml
+                 echo "Finding HPO terms"
+                 HPO=`ls -t /hpf/largeprojects/tgnode/sandbox/mcouse_analysis/HPO/${project}/${project_family}* | head -n 1` # get most recent HPO file
+                 echo $HPO
+                 if [ -z $HPO ]; then
+                         echo "No HPO terms found"
+                 fi
+                 sed -i "s+hpo: \"\"+hpo: \"${HPO}\"+"  ${ANALYSIS_DIR}/${project_family}/PacBio/config.yaml
 
                 # add pedigree to config.yaml
-                if [ "$project" = "DECODER" ]; then
-                        echo "Finding pedigree"
-                        ped=`ls /hpf/largeprojects/tgnode/sandbox/mcouse_analysis/pedigrees/${project}/${project_family}* | tail -n 1` # last file is the most recent
-                        echo $ped
-                        if [ -z $ped ]; then
-                                echo "No pedigree found"
-                        fi
-                        sed -i "s+ped: \"\"+ped: \"${ped}\"+"  ${ANALYSIS_DIR}/${project_family}/PacBio/config.yaml
-                fi
+                 if [ "$project" = "DECODER" ]; then
+                         echo "Finding pedigree"
+                         ped=`ls /hpf/largeprojects/tgnode/sandbox/mcouse_analysis/pedigrees/${project}/${project_family}* | tail -n 1` # last file is the most recent
+                         echo $ped
+                         if [ -z $ped ]; then
+                                 echo "No pedigree found"
+                         fi
+                         sed -i "s+ped: \"\"+ped: \"${ped}\"+"  ${ANALYSIS_DIR}/${project_family}/PacBio/config.yaml
+                 fi
 
 
 		# not C4R, so remove C4R sample columns from reports
@@ -93,11 +103,25 @@ do
                 echo -e "family\tplatform\tsmall_variant_vcf\ttrgt_vcf_dir\tpbsv_vcf\ttrgt_pathogenic_vcf_dir" > ${ANALYSIS_DIR}/${project_family}/PacBio/units.tsv
                 deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}-cohort.joint.GRCh38.small_variants.phased.vcf.gz
                 if [ ! -f $deepvariant ]; then
-                        deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.small_variants.phased.vcf.gz
+                        deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}.joint.GRCh38.small_variants.phased.vcf.gz
+                        if [ ! -f $deepvariant ]; then
+                                tcag_family=`echo $family | sed 's/DSK_/DSK_DNA_/'`
+                                deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${tcag_family}-cohort.joint.GRCh38.small_variants.phased.vcf.gz
+                                if [ ! -f $deepvariant ]; then
+                                        deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.small_variants.phased.vcf.gz # singleton sample
+                                fi
+                        fi
                 fi
                 sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}-cohort.joint.GRCh38.structural_variants.phased.vcf.gz
                 if [ ! -f $sv ]; then
-                        sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.structural_variants.phased.vcf.gz
+                        sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}.joint.GRCh38.structural_variants.phased.vcf.gz
+                        if [ ! -f $sv ]; then
+                                tcag_family=`echo $family | sed 's/DSK_/DSK_DNA_/'`
+                                sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${tcag_family}-cohort.joint.GRCh38.structural_variants.phased.vcf.gz
+                                if [ ! -f $sv ]; then
+                                        sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.structural_variants.phased.vcf.gz  # singleton sample
+                                fi
+                        fi
                 fi
                 echo -e "$project_family\tPACBIO\t${deepvariant}\ttrgt\t${sv}\t" >> ${ANALYSIS_DIR}/${project_family}/PacBio/units.tsv
 
@@ -105,7 +129,7 @@ do
                 mkdir ${ANALYSIS_DIR}/${project_family}/PacBio/cnv
 
                 # create trgt directory
-                mkdir ${ANALYSIS_DIR}/${project_family}/PacBio/trgt
+                mkdir ${ANALYSIS_DIR}/${project_family}/PacBio/trgt 
         fi
 
         # add sample and bams
@@ -120,6 +144,9 @@ do
         echo "Copying CNV for $sequence_id"
         CNV=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.hificnv_annPipelineRevv1.7.1_20250120_hg38_PACBIO_cnv.tagged.tsv
         CNV_filename=`basename $CNV`
+        if [ ! -d ${ANALYSIS_DIR}/${project_family}/PacBio/cnv ]; then
+                mkdir -p ${ANALYSIS_DIR}/${project_family}/PacBio/cnv
+        fi
         cp $CNV ${ANALYSIS_DIR}/${project_family}/PacBio/cnv
         # rename CNV file
         sequence_id_no_period=`echo $sequence_id | tr '.' '_'` # TCAG replaces period in DECODER IDs with underscore
@@ -131,7 +158,10 @@ do
         trgt=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.trgt.sorted.phased.vcf.gz
         echo $trgt
         echo "Copying trgt for $sequence_id"
-        cp $trgt ${ANALYSIS_DIR}/${project_family}/PacBio/trgt
+        if [ ! -d ${ANALYSIS_DIR}/${project_family}/PacBio/trgt ]; then
+                mkdir -p ${ANALYSIS_DIR}/${project_family}/PacBio/trgt
+        fi
+        cp ${trgt}* ${ANALYSIS_DIR}/${project_family}/PacBio/trgt
         trgt=`basename $trgt`
         trgt=${ANALYSIS_DIR}/${project_family}/PacBio/trgt/$trgt
 
@@ -139,11 +169,25 @@ do
         echo "Replacing sample IDs in VCFs"
         deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}-cohort.joint.GRCh38.small_variants.phased.vcf.gz
         if [ ! -f $deepvariant ]; then
-                deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.small_variants.phased.vcf.gz
+                deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}.joint.GRCh38.small_variants.phased.vcf.gz
+                if [ ! -f $deepvariant ]; then
+                        tcag_family=`echo $family | sed 's/DSK_/DSK_DNA_/'`
+			deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${tcag_family}-cohort.joint.GRCh38.small_variants.phased.vcf.gz
+			if [ ! -f $deepvariant ]; then
+                                deepvariant=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.small_variants.phased.vcf.gz # singleton sample
+			fi
+                fi
         fi
         sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}-cohort.joint.GRCh38.structural_variants.phased.vcf.gz
         if [ ! -f $sv ]; then
-                sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.structural_variants.phased.vcf.gz
+                sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${family}.joint.GRCh38.structural_variants.phased.vcf.gz
+                if [ ! -f $sv ]; then
+                        tcag_family=`echo $family | sed 's/DSK_/DSK_DNA_/'`
+                        sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${tcag_family}-cohort.joint.GRCh38.structural_variants.phased.vcf.gz
+			if [ ! -f $sv ]; then
+                                sv=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${project}/${sequence_id}.GRCh38.structural_variants.phased.vcf.gz  # singleton sample
+			fi
+                fi
         fi
         # ID map file
         echo $sequence_id_no_period $project_id > sample_rename.txt
@@ -162,12 +206,29 @@ do
         fi
 done<$analyses
 
-#submit one CNV merge job per family
-for project_family in `awk '{print $3}' $analyses | cut -d '.' -f1 | tr -d '_' |  uniq`
+for project_family in `awk '{print $3}' $analyses | cut -d '.' -f1 |  uniq`
 do
          echo $project_family
-         project_family=$(echo "$project_family" | tr -d '\r')
-         if [ "$project_family" != "DecoderID" ]; then
+         if [ "$project_family" != "Decoder_ID" ]; then
+              # check if all samples in analysis TSV are represented in the pedigree 
+              ped_family=`echo $project_family | tr -d '_'`
+              ped=`ls /hpf/largeprojects/tgnode/sandbox/mcouse_analysis/pedigrees/${project}/${ped_family}* | tail -n 1`
+              # get all samples in the pedigree
+              samples=`awk '{print $2}' $ped`
+              # check if all samples in analysis TSV are represented in the pedigree
+              for sample in `grep  $project_family $analyses | grep LRWGS |  awk '{print $3}' |  tr -d '_' | tr '.' '_'`; do
+                  if ! echo $samples | grep -q $sample; then
+                      echo "Sample $sample not found in pedigree for $project_family, exiting"
+                      exit 1
+                  fi
+              done
+              # exclude TRGT de novo rule if family is not a trio 
+              project_family=$(echo "$project_family" | tr -d '_' | tr -d '\r')
+              if [ $(wc -l ${ANALYSIS_DIR}/${project_family}/PacBio/samples.tsv | awk '{print $1}') -lt 4 ]; then
+                echo "Family $project_family is not a trio, excluding TRGT de novo rule"
+                sed -i "s+{SLURM}+{SLURM} -p sv/${project_family}.pbsv.csv small_variants/coding/${project_family} small_variants/panel/${project_family} small_variants/panel-flank/${project_family}   pathogenic_repeats/${project_family}.known.path.str.loci.csv repeat_outliers/${project_family}.repeat.outliers.annotated.csv+g" ${ANALYSIS_DIR}/${project_family}/PacBio/crg2-pacbio.sh
+              fi
+              # submit one CNV merge job per family
               cd ${ANALYSIS_DIR}/${project_family}/PacBio/cnv
               echo "Submitting CNV merge job"
               sbatch ${CRG2_PACBIO}/scripts/merge.cnv.reports.sh $project_family
