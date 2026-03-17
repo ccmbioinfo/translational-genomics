fe1c350cfe9d49a7769d98c936857ef8370d2857
Madeline Couse <mhcouse@gmail.com>
2026-03-17 10:27:48 -0400

    add compatibility with older PacBio pipeline file naming

--- DNAseq_scripts/query_files_from_irods.sh
+++ DNAseq_scripts/query_files_from_irods.sh
@@ -7,22 +7,21 @@ OUT=/hpf/largeprojects/tgnode/sandbox/mcouse_analysis/files_from_irods/${PROJECT
 echo "Filepaths will be saved to $OUT"
 
 # retrieve deep variant joint-genotyped VCF
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.small_variants.phased.vcf.gz'" > $OUT
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.small_variants.phased.vcf.gz.tbi'" >> $OUT
+{ iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.small_variants.phased.vcf.gz%'" | grep 'fam\|cohort' > $OUT; } || { iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.deepvariant.phased.vcf.gz%'" | grep 'fam\|cohort' > $OUT; }
 
 # retrieve joint-genotyped SV VCF
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.structural_variants.phased.vcf.gz'" >> $OUT
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.structural_variants.phased.vcf.gz.tbi'" >> $OUT
+{ iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.structural_variants.phased.vcf.gz%'" | grep 'fam\|cohort' >> $OUT; } || { iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.pbsv.phased.vcf.gz%'" | grep 'fam\|cohort' >> $OUT; }
 
 # retrieve BAMs
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.haplotagged.bam'" >> $OUT
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.haplotagged.bam.bai'" >> $OUT
+{ iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.haplotagged.bam%'" >> $OUT; } || { iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%GRCh38.aligned.haplotagged.bam%'" >> $OUT; }
 
 # retrieve CNV VCFs
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%hificnv.vcf.gz%'" >> $OUT
+{ iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%hificnv.vcf.gz%'" >> $OUT; } || { iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like 'hificnv%.vcf.gz%'" >> $OUT; }
 
 # retrieve TRGT VCFs and spanning BAMs
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%trgt.sorted.phased.vcf.gz'" >> $OUT
-iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%trgt.sorted.phased.vcf.gz.tbi'" >> $OUT   
+{ iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%trgt.sorted.phased.vcf.gz%'" >> $OUT; } || { iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%trgt.sorted.vcf.gz%'" >> $OUT; }
 iquest "%s/%s" "SELECT COLL_NAME, DATA_NAME where COLL_NAME like '%$LIMSID%' and DATA_NAME like '%.trgt.spanning.sorted.bam%'" >> $OUT
+
+grep -v CAT_NO_ROWS_FOUND $OUT > $OUT.tmp
+mv $OUT.tmp $OUT
 
