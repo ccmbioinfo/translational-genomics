6bb7fc3da43faa3218bb7bc3e7d07fa8d9ef0855
Madeline Couse <mhcouse@gmail.com>
2026-07-08 13:57:12 -0400

    add archive scripts

---
+++ archive_scripts/01_irsync_to_iRODS.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=irsync-to-irods
+#SBATCH --time=50:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --mail-user=madeline.couse@sickkids.ca
+#SBATCH --mail-type=END,FAIL
+
+# This script archives directories on Isilon/PowerScale to iRODS.
+# Note that it does not automatically delete the local files after syncing to iRODS. This can be done manually after the script finishes without error, but first run final_check.sh. 
+# Usage: sh 01_irsync_to_iRODS.sh <local_path> <archival_path>
+# Example usage: sbatch irsync_to_iRODS.sh CHU_SJ /resarchivezone/tgnode/external_datasets/CHUSJ
+
+set -euo pipefail
+
+local_path=$1 # local path on Isilon/PowerScale
+archival_path=$2 # tgnode iRODS base path: /resarchivezone/tgnode/
+
+# load iRODS module
+module load irods_client/4.3.1
+
+# set iRODS environment variables
+export IRODS_ENVIRONMENT_FILE=/home/mcouse/.irods/irods_environment.json
+
+if [ -z "$local_path" ] || [ -z "$archival_path" ]; then
+    echo "Usage: $0 <local_path> <archival_path>"
+    exit 1
+fi
+
+if [ ! -d "$local_path" ]; then
+    echo "Local path $local_path does not exist"
+    exit 1
+fi
+
+# sync the files to iRODS with -K flag for checksum verification
+echo "syncing files from $local_path to $archival_path..."
+irsync -rvK $local_path i:$archival_path
+echo "irsync complete, syncing again to be safe..."
+
+# just to be safe, sync the files to iRODS again (not sure if this is still an issue, https://github.com/irods/irods/issues/2822)
+irsync -rvK $local_path i:$archival_path
+echo "irsync complete!"
---
+++ archive_scripts/README.md
@@ -0,0 +1,20 @@
+
+# Archiving data to iRODS
+
+`01_irsync_to_iRODS.sh`
+
+This script archives directories on Isilon/PowerScale to iRODS using `irsync` with the `-K` flag to perform checksums. Likely overkill, but it performs `irsync` twice to verify that the files have been archived appropriately. 
+
+Usage: ` 01_irsync_to_iRODS.sh <local_path> <archival_path>`
+
+`02_final_check.sh`
+
+This script compares the numbers of files in the directory on Isilon/PowerScale to the corresponding directory on iRODS. The number of files in the local directory, multiplied by two, should equal the number of files in the iRODS directory (each file in iRODS is mirrored). If the check is `OK`, you can proceed with removing the files locally that you have just archived. If the file outputs a `WARNING`, try rsyncing your files again.
+
+Usage: `02_final_check.sh <local_path> <archival_path>`
+
+Example output:
+
+| iRODS_count_with_mirror |	Isilon_count_x2 | check |
+| ----------------------- | --------------- | ------|
+| 754 | 754 | OK | 
